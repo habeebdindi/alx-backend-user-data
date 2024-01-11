601225cc211f789c2520425750f0123ddc131130
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-01-11 03:39:54 +0100

    Add task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,3 +31,14 @@ def filter_datum(fields: List[str], redaction: str,
     patt = re.compile(r'(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)')
     return re.sub(patt, lambda y: y.group(1) + '=' + redaction
                   if y.group(1) in fields else y.group(0), message)
+
+def get_logger() -> logging.Logger:
+    """ Takes no arguments and return a logger object """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    sh = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    sh.setFormatter(formatter)
+    logger.addHandler(sh)
+    return logger
