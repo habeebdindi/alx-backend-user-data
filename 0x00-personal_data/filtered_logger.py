177f5022f5a29c55f3e5142b36190cc6ddfb0337
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-01-11 02:30:08 +0100

    Use re

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-This module contains a function
-"""
+"""This module contains a function"""
 import re
 from typing import List
 
@@ -10,4 +8,4 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """This function returns the log message obfuscated"""
     pattern = re.compile(r'(' + '|'.join(fields) + r')=([^;]+)')
-    return pattern.sub(r'\1={}'.format(redaction), message)
+    return re.sub(pattern, lambda y: y.group(1) + '=' + redaction, message)
