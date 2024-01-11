034416ba1eae19c5c8ac2dc29341baa7c7f16a01
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-01-11 03:34:52 +0100

    Add task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,28 @@
 """This module contains a function"""
 import re
 from typing import List
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initializes the instance"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ilter values in incoming log records using filter_datum
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
