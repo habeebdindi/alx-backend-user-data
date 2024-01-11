2bc85ff4f01f5821bb50cbc8eda9ad426f3f8398
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-01-11 02:03:01 +0100

    Fix pep8

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """This function returns the log message obfuscated"""
     pattern = re.compile(r'(' + '|'.join(fields) + r')=([^;]+)')
     return pattern.sub(r'\1={}'.format(redaction), message)
