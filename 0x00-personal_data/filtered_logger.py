3eac31f0823989851976454da2981550abe1e2ee
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-01-11 02:57:17 +0100

    Fix grouping issue in regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,5 +7,6 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """This function returns the log message obfuscated"""
-    pattern = re.compile(r'(' + '|'.join(fields) + r')=([^;]+)')
-    return re.sub(pattern, lambda y: y.group(1) + '=' + redaction, message)
+    patt = re.compile(r'(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)')
+    return re.sub(patt, lambda y: y.group(1) + '=' + redaction
+                  if y.group(1) in fields else y.group(0), message)
