c547f63b93de379ffdcc30f3018b09b5a5321454
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-01-16 23:16:10 +0100

    Add auth template

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,3 +1,26 @@
 #!usr/bin/env python3
 """Auth module for the API
 """
+from flask import request
+
+
+class Auth:
+    """ Defines the auth class
+    """
+    def __init__(self):
+        """ Initializes the class
+        """
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Returns False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Returns None
+        """
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Returns None
+        """
