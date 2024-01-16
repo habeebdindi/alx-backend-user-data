12b2b32c98f7a23e453212c56bdd0001d1a80491
Habeeb Dindi <habeebullahdindi@gmail.com>
2024-01-16 15:46:38 +0100

    Implement unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> None:
+    """ GET /api/v1/unauthorized
+    Return:
+      - None
+    """
+    abort(401)
